677fce589f4e8216404d1360b4b82f93601b84bd
test <slam67@ajou.ac.kr>
2017-12-07 23:44:29 +0900

    팀정보길게누르면 팀정보액티비티로넘어감 대회정보길게누르면 대회정보액티비티로넘어감

---
+++ app/src/main/java/onc/appproject/firstonc/TeamInfoActivity.java
@@ -0,0 +1,13 @@
+package onc.appproject.firstonc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_info);
+    }
+}
